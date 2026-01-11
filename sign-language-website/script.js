d5ab9c1d444060227119475afe822af596881346
m-12822282-cpu <m-12822282@moe-dl.edu.my>
2026-01-11 19:42:58 +0800

    Update script.js

--- sign-language-website/script.js
+++ sign-language-website/script.js
@@ -7,7 +7,7 @@ function translateText() {
   for (let char of input) {
     if (char >= "a" && char <= "z") {
       const img = document.createElement("img");
-      img.src = "images/" + char + ".png";
+      img.src = "sign-language-website/images/" + char + ".png";
       img.alt = char;
       img.style.width = "60px";
       img.style.margin = "5px";
@@ -15,4 +15,6 @@ function translateText() {
     }
   }
 }
+
+
 
