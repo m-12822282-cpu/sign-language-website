7f08d5d1c52f7366566a7b068c0abef9a15fd545
m-12822282-cpu <m-12822282@moe-dl.edu.my>
2026-01-11 20:31:17 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+function translateText() {
+  const input = document.getElementById("textInput").value.toLowerCase();
+  const output = document.getElementById("output");
+
+  output.innerHTML = "";
+
+  for (let char of input) {
+    if (char >= "a" && char <= "z") {
+      const img = document.createElement("img");
+      img.src = "sign-language-website/images/" + char + ".png";
+      img.alt = char;
+      img.style.width = "60px";
+      img.style.margin = "5px";
+      output.appendChild(img);
+    }
+  }
+}
+
+
+
